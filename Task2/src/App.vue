0f962238c5181a57c0dba4a7e1aa8e28657b255c
Sergio-Serrallonga <sergio.serrallonga@theksquaregroup.com>
2024-06-08 12:02:46 -0600

    feat: complete task2

--- Task2/src/App.vue
+++ Task2/src/App.vue
@@ -1,21 +1,18 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <h1>Prueba técnica de ingreso</h1>
+      <br />
+      Candidato: Sergio Serrallonga Hernandez
     </div>
   </header>
 
-  <main>
-    <TheWelcome />
-  </main>
+  <main><Home /></main>
 </template>
+
+<script setup>
+import Home from "./components/Home.vue";
+</script>
 
 <style scoped>
 header {
@@ -41,6 +38,7 @@ header {
   header .wrapper {
     display: flex;
     place-items: flex-start;
+    flex-direction: column;
     flex-wrap: wrap;
   }
 }
---
+++ Task2/src/components/CardItem.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="cards-container">
+    <div class="card" v-for="card in cards">
+      <img :src="card.imageUrl" />
+      <p>Name: {{ card.name }}</p>
+      <p>Color: {{ card.colors }}</p>
+      <p>Type: {{ card.type }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  props: {
+    cards: {
+      type: Object,
+    },
+  },
+});
+</script>
+<style scoped>
+.cards-container {
+  display: flex;
+  justify-content: space-evenly;
+  flex-wrap: wrap;
+  gap: 10px;
+}
+
+.cards-container .card {
+  width: 152px;
+  overflow: hidden;
+  text-overflow: ellipsis;
+}
+</style>
